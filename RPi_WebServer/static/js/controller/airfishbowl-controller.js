af9ea74477d55ae19e6912427e3848a4af3b3206
Carlos Baraza Haro <carlos.baraza@ieee.org>
2013-03-13 15:55:39 +0100

    Add arrows to design with js animation

--- RPi_WebServer/static/js/controller/airfishbowl-controller.js
+++ RPi_WebServer/static/js/controller/airfishbowl-controller.js
@@ -11,21 +11,25 @@ $(document).keydown(function(e){
     if (e.keyCode == 37) { 
        arrows_bk_default();
        $('.controller-right').addClass('pressed-left');
+       $('.controller-arrows-left').addClass('controller-arrows-pressed');
        return false;
     }
     if (e.keyCode == 38) { 
        arrows_bk_default();
        $('.controller-right').addClass('pressed-up');
+       $('.controller-arrows-up').addClass('controller-arrows-pressed');
        return false;
     }
     if (e.keyCode == 39) { 
        arrows_bk_default();
        $('.controller-right').addClass('pressed-right');
+       $('.controller-arrows-right').addClass('controller-arrows-pressed');
        return false;
     }
     if (e.keyCode == 40) { 
        arrows_bk_default();
        $('.controller-right').addClass('pressed-down');
+       $('.controller-arrows-down').addClass('controller-arrows-pressed');
        return false;
     }
 });
@@ -33,18 +37,22 @@ $(document).keydown(function(e){
 $(document).keyup(function(e){
     if (e.keyCode == 37) {
        $('.controller-right').removeClass('pressed-left');
+       $('.controller-arrows-left').removeClass('controller-arrows-pressed');
        return false;
     }
     if (e.keyCode == 38) { 
        $('.controller-right').removeClass('pressed-up');
+       $('.controller-arrows-up').removeClass('controller-arrows-pressed');
        return false;
     }
     if (e.keyCode == 39) { 
        $('.controller-right').removeClass('pressed-right');
+       $('.controller-arrows-right').removeClass('controller-arrows-pressed');
        return false;
     }
     if (e.keyCode == 40) { 
        $('.controller-right').removeClass('pressed-down');
+       $('.controller-arrows-down').removeClass('controller-arrows-pressed');
        return false;
     }
 });
